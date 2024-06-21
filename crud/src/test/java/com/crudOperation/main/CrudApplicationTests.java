948ec271722727f4874b24a6a605ba7825130d9b
cisco-ranajit <ranajitkhandual@gmail.com>
2024-06-21 23:50:00 +0530

    project files added

---
+++ crud/src/test/java/com/crudOperation/main/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudOperation.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
