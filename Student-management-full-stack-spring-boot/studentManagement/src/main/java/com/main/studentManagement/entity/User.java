2dec5e1081106382987b12bfc059c0b5cbe5c4e3
cisco-ranajit <ranajitkhandual@gmail.com>
2024-06-27 09:30:43 +0530

    id remove

--- Student-management-full-stack-spring-boot/studentManagement/src/main/java/com/main/studentManagement/entity/User.java
+++ Student-management-full-stack-spring-boot/studentManagement/src/main/java/com/main/studentManagement/entity/User.java
@@ -5,22 +5,14 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(unique = true)
     private String username;
     private String password;
 
@@ -80,12 +72,6 @@ public class User implements UserDetails {
     }
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
     }
     public String getUsername() {
         return username;
