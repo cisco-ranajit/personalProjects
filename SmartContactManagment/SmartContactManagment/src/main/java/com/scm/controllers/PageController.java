a94498d1e53fa503ceb314af4a8e506b9448f0b5
cisco-ranajit <ranajitkhandual@gmail.com>
2024-06-28 10:26:21 +0530

    tailwind css added

--- SmartContactManagment/SmartContactManagment/src/main/java/com/scm/controllers/PageController.java
+++ SmartContactManagment/SmartContactManagment/src/main/java/com/scm/controllers/PageController.java
@@ -2,12 +2,15 @@ package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("title", "Spring Boot Application");
         return "home";
     }
+    
 }
