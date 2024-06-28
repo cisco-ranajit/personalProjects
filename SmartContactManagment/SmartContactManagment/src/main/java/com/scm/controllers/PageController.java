b28c99db08b56c3e272457416e1f55f5f3d9200a
cisco-ranajit <ranajitkhandual@gmail.com>
2024-06-28 10:49:12 +0530

    added

--- SmartContactManagment/SmartContactManagment/src/main/java/com/scm/controllers/PageController.java
+++ SmartContactManagment/SmartContactManagment/src/main/java/com/scm/controllers/PageController.java
@@ -1,13 +1,15 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("title", "Spring boot application");
         return "home";
     }
 }
